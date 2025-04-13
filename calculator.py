49880bd35e1b663cb77e02c20a7566348cff666a
birkenn <birkenn@icloud.com>
2025-04-12 22:40:57 -0400

    nice

--- calculator.py
+++ calculator.py
@@ -1,11 +1,17 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
-
-One function per operation, in order.
-"""
-# First example
+#CREATED BY NOAH BIRKEN and CHRISOPHER MEZA
+import math
 def add(a, b): 
-    pass
-
-
+    return a+b
+def subtract(a, b):
+    return a-b
+def multiply(a, b):
+    return a*b
+def divide(a, b):
+    try:
+        return b/a
+    except ZeroDivisionError as error:
+        print(str(error))
+def logarithm(a,b):
+    return math.log(b,a)
+def exponent(a,b):
+    return a**b
