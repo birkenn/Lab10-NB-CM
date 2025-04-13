87a49af18b282bc5529dc359942c4ac22e239adb
birkenn <birkenn@icloud.com>
2025-04-13 01:06:35 -0400

    nice

--- calculator.py
+++ calculator.py
@@ -1,1 +1,19 @@
-
+import math
+def add(a, b):
+    return a+b
+def subtract(a, b):
+    return a-b
+def multiply(a, b):
+    return a*b
+def divide(a, b):
+    try:
+        return b/a
+    except ZeroDivisionError as error:
+        print(str(error))
+def logarithm(a,b):
+    try:
+        return math.log(b,a)
+    except ValueError as error:
+        print(str(error))
+def exponent(a,b):
+    return a**b
