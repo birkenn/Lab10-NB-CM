da86dd088924dd4bccbe3221d86ece7f5e9397f0
birkenn <birkenn@icloud.com>
2025-04-13 01:17:44 -0400

    nice

--- calculator.py
+++ calculator.py
@@ -1,11 +1,19 @@
 import math
+def square_root(a):
+    try:
+        return math.sqrt(a)
+    except ValueError as error:
+        print(str(error))
+def hypotenuse(a,b):
+    math.hypot(a,b)
+
 def add(a, b):
     return a+b
 def subtract(a, b):
     return a-b
-def multiply(a, b):
+def mul(a, b):
     return a*b
-def divide(a, b):
+def div(a, b):
     try:
         return b/a
     except ZeroDivisionError as error:
@@ -15,5 +23,5 @@ def logarithm(a,b):
         return math.log(b,a)
     except ValueError as error:
         print(str(error))
-def exponent(a,b):
+def exp(a,b):
     return a**b
